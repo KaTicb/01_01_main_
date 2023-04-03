fa37c3cbd5c4128be244940dfe1222de436ef3d5
KaTicb <trick1204ed@mail.ru>
2023-04-03 22:00:56 +0300

    nicht

--- cmake-build-debug/CMakeFiles/01_01_main_.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for 01_01_main_.
-# This may be replaced when dependencies are built.
--- main.cpp
+++ main.cpp
@@ -1,8 +1,49 @@
 #include <iostream>
+#include <string>
 
-int main(){
+int main(int argc, char** argv, char** env){
 
-    std::cout<<0<<std::endl;
-    return 1;
+    std::cout<<"argc"<<std::endl<<std::endl;
+    std::cout<<argc<<std::endl<<std::endl;
+
+//    std::cout<<"argv"<<std::endl<<std::endl;
+//    std::string string_argv;
+//    std::string string_env;
+//    for (int i=0;i<argc;i++){
+//        string_argv += argv[i];
+//    }
+//
+//    std::cout<<string_argv<<std::endl<<std::endl;
+//    for (int i=0;env[i];i++){
+//        string_env += env[i];
+//    }
+
+  //  std::cout<<string_env<<std::endl<<std::endl;
+    return 0;
 
 }
+
+//#include <sys/syscall.h>
+//#include <unistd.h>
+//
+//int print(const char* message, int size)
+//{
+//    int exit_code = 0;
+//    syscall(SYS_write, 1, message, size);
+//    return exit_code;
+//}
+//
+//int main(int /*argc*/, char* /*argv*/ [])
+//{
+//    return print("hello world\n", 12);
+//}
+//
+//extern "C"
+//{
+//void _start()
+//{
+//    int result_code = main(0, nullptr);
+//    syscall(SYS_exit, result_code);
+//}
+//}
+
